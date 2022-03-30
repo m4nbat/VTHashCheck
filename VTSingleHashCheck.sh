3bca683435eadcde3b4325edbe5d74fd70f86d1e
m4nbat <g.r.j.knapp@gmail.com>
2022-03-30 01:12:20 +0100

    Update VTSingleHashCheck.sh

--- VTSingleHashCheck.sh
+++ VTSingleHashCheck.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-echo 'enter hash'
+echo enter your hash
 read hash
-echo "$hash"
-echo 'enter output filename'
-read $outputfile
-curl --request GET --url "https://www.virustotal.com/vtapi/v2/file/report?apikey=<YOUR API KEY HERE>&resource=$hash"  | jq '.' > $outputfile
+echo enter your api key
+read apikey
+echo enter output file path
+read outputfile
+echo "You have entered the following API key:"$apikey" and "$hash" combination that will be stored in "$outputfile""
+curl --request GET --url "https://www.virustotal.com/vtapi/v2/file/report?apikey="$apikey"&resource="$hash""  | jq '.' > $outputfile
 sleep 2
 tail -n 13 $outputfile

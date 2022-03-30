e169fae5e6a3dc56ae84d4c00337f6746db6a136
m4nbat <g.r.j.knapp@gmail.com>
2022-03-30 01:14:01 +0100

    Update VTMultiHashCheck.sh

--- VTMultiHashCheck.sh
+++ VTMultiHashCheck.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+echo enter your api key
+read apikey
 echo 'please enter the path of your hashes file'
 read hashfile
 echo 'please enter your output filename'
 read outputfile
 while read -r hash; do
 echo "$hash"
-curl --request GET --url "https://www.virustotal.com/vtapi/v2/file/report?apikey=<YOUR API KEY>&resource=$hash"  | jq '.' >> $outputfile
+curl --request GET --url "https://www.virustotal.com/vtapi/v2/file/report?apikey="$apikey"&resource="$hash""  | jq '.' >> $outputfile
 done <"$hashfile"
 sleep 2
 egrep '(scan_id|sha256|sha1|md5|scan_date|permalink|total|positives)' $outputfile
